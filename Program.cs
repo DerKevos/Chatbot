2bb076893b1d645f482aa653f33d87ebf3662b87
Der_Kevos <der.kevos@gmail.com>
2021-09-07 21:19:14 +0200

    Moin

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chatbot
+{
+    class Program
+    {
+        static void Main()
+
+        { var myDict = new Dictionary<string, string>
+            {
+                { "Festplatte", "Garantie" },
+                { "Internet", "Nicht mein Problem" }
+            };
+            Console.WriteLine("Guten Tag \nWie kann ich ihnen Helfen?");
+            String benutzereingabe;
+            do
+            {
+                benutzereingabe = Console.ReadLine();
+            } while (benutzereingabe == "");
+
+            if (benutzereingabe != "bye")
+            {string[] wordarray = benutzereingabe.Split(' ');
+                foreach (string word in wordarray)
+                {
+                    foreach (var dictEntrie in myDict)
+                    {
+                        if (dictEntrie.Key == word)
+                        {Console.WriteLine(dictEntrie.Value);
+                            
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
