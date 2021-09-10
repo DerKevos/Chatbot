f7752a6e5dfb397c078cf282e121456430c136e7
la-kr <la-kr@LAPTOP-QMO0GHTF>
2021-09-10 09:25:26 +0200

    Dictionary gegen offizielle Version augetauscht

--- Program.cs
+++ Program.cs
@@ -97,9 +97,94 @@ namespace Chatbot
             //Es ist ein Nachschlagewerk wo man Key-Value-Pair hinzufügen und speichern kann.
             var myDict = new Dictionary<string, string>
             {
-                {"Festplatte", "Garantie"},
-                {"Internet", "Nicht mein Problem :)"},
-                {"Monitor", "Schon mal neugestartet"},
+                {
+                    "Festplatte",
+                    "Probleme mit der Festplatte gehen meistens mit dem Speicherplatz einher, daher sollte man darauf achten, ungenutzte Daten zu bereinigen oder auf unseren virtuellen Laufwerken zu speichern."
+                },
+                {
+                    "Internet",
+                    "Die meisten internetbedingten Probleme können durch einen Routerneustart gelöst werden. Kappen sie dafür den Router für ca. 1 Minute vom Strom und versuchen Sie es danach erneut."
+                },
+                {
+                    "Passwort",
+                    "Um ihr Passwort zurückzusetzten müssen sich sich per E-Mail mit ihrer Mitarbeiter-ID bei unserer Support-Mail gambit-support@outlook.com melden."
+                },
+                {
+                    "Monitor",
+                    "Sollte der Monitor kein Bild anzeigen, kann es helfen, die Kabel einmal neu zu verbinden. Sollte dies nicht erfolgreich sein, melden Sie sich bitte telefonisch oder per Mail unter gambit-support@outlook.com"
+                },
+                {
+                    "Bildschirm",
+                    "Sollte der Bildschirm kein Bild anzeigen, kann es helfen die Kabel einmal neu zu verbinden. Sollte dies nicht erfolgreich sein, melden Sie sich bitte telefonisch oder per Mail unter gambit-support@outlook.com"
+                },
+                { "Tastatur", "Bitte versuchen die Peripherie einmal neu anzuschließen." },
+                { "Englisch", "Die Anzeigesprache können sie in den Systemeinstellungen ändern." },
+                { "Akku", "Einen neuen Akku können sie selbstständig in dem Bestellportal bestellen." },
+                {
+                    "Azubi",
+                    "Um einen neuen Azubi mit Hardware auszustatten, melden Sie sich bitte bei unserem Kollegen. Herr Mustermann Tel. 0123 456 789"
+                },
+                {
+                    "Handy",
+                    "Ein neues Firmenhandy, sowie eine Vertragserweiterung oder zusätzliche Hardware können Sie über ihren Vorgesetzten beantragen."
+                },
+                {
+                    "Kaputt",
+                    "Sollte Hardware aus Eigenverschuldung beschädigt werden, muss der Ersatz oder die Reparatur über den Vorgesetzten beantragt werden. In anderen Fällen müssen Sie die Hardware zu uns bringen, damit wir die Garantie einlösen können."
+                },
+                {
+                    "Postfach",
+                    "Ein neues Postfach können Sie in Outlook unter den Kontoeinstellungen hinzufügen. Sollten Sie dabei Hilfe benötigen melden Sie sich bitte telefonisch oder per Mail unter gambit-support@outlook.com"
+                },
+                {
+                    "wiederherstellen",
+                    "Um eine Datei wiederherstellen zu können, müssen Sie uns per Mail den Dateipfad und die gewünschte Wiederherstellungszeit nennen. Die Mailadresse dafür lautet gambit-support@outlook.com"
+                },
+                {
+                    "restore",
+                    "Um eine Datei wiederherstellen zu können, müssen Sie uns per Mail den Dateipfad und die gewünschte Wiederherstellungszeit nennen. Die Mailadresse dafür lautet gambit-support@outlook.com"
+                },
+                { "WLAN", "Das WLAN passwort kann unter folgender Adresse gefunden werden: https://wlan.gambit.de" },
+                {
+                    "sichern",
+                    "Um ihre Daten zu sichern im Falle eines Rechnerwechseln o.Ä. können sie unsere Firmeninterne Software EasySync benutzen. Diese überträgt ihre Daten auf unsere virtuellen Laufwerke."
+                },
+                {
+                    "Headset",
+                    "Um ein für Sie passendes Headset auszuwählen, bitten wir Sie sich telefonisch zu melden. Tel. 0123 456 789"
+                },
+                {
+                    "langsam",
+                    "In diesem Fall kann es helfen, den Rechner einmal über den Neustart-Button im Menu neuzustarten. Sollten danach weitere Probleme auftreten, melden sie sich bitte per Mail oder telefonisch bei uns."
+                },
+                {
+                    "Virus",
+                    "Sollten sie vermuten, sich einen Virus auf ihren Rechner geholt zu haben, müssen Sie schnellstmöglich die Nutzung des Geräts einstellen und es bei uns vor Ort vorbeibringen. Melden Sie sich auch bitte umgehend bei uns damit wir sichergehen können, dass unser Betrieb keine Schäden davonträgt! Tel. 0123 456 789"
+                },
+                {
+                    "fragwürdige",
+                    "Sollten Sie auf einen Link in einer fragwürdigen Mail geklickt haben stellen Sie bitte umgehend die Nutzung des Gerätes ein und melden sich bei uns per Telefon! Tel. 0123 456 789."
+                },
+                {
+                    "Software",
+                    "Neue Software sollten Sie niemals selber installieren, sondern unser firmeninternes Softwarecenter nutzen. Wenn gewünschte Software nicht vorhanden ist, melden Sie dies bitte bei uns, damit wir das prüfen können."
+                },
+                {
+                    "Excel",
+                    "Excel kann wie alle anderen Office-Programme von allen Mitarbeiter genutzt werden. Sollten Sie Schwierigkeiten haben eine Excel-Datei zu öffnen, stellen Sie bitte sicher, dass die Datei noch an der Stelle liegt, an der Sie diese abgespeichert haben."
+                },
+                {
+                    "Word",
+                    "Word kann wie alle anderen Office-Programme von allen Mitarbeiter genutzt werden. Sollten Sie Schwierigkeiten haben eine Word-Datei zu öffnen, stellen Sie bitte sicher, dass die Datei noch an der Stelle liegt, an der Sie diese abgespeichert haben."
+                },
+                {
+                    "PowerPoint",
+                    "PowerPoint kann wie alle anderen Office-Programme von allen Mitarbeiter genutzt werden. Sollten Sie Schwierigkeiten haben eine PowerPoint-Datei zu öffnen, stellen Sie bitte sicher, dass die Datei noch an der Stelle liegt, an der Sie diese abgespeichert haben."
+                },
+                {
+                    "Office",
+                    "Sollte Sie Probleme mit einer Office Anwendung haben (Outlook, Excel, PowerPoint, Word, etc.) führen Sie bitte im Softwarecenter die Office-Reparatur durch."
+                },
             };
             return myDict;
         }
