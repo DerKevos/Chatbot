c69575378c2ae82dbbe3dc70418642c5e5d298b1
la-kr <la-kr@LAPTOP-QMO0GHTF>
2021-09-10 09:23:38 +0200

    Rechtschreibkorrektur

--- Program.cs
+++ Program.cs
@@ -8,45 +8,45 @@ namespace Chatbot
         static void Main()
         {
             var myDict = CreateDictionary();
-            //Erste ausgabe vom Chatbot
+            //Erste Ausgabe vom Chatbot
             Console.WriteLine("Guten Tag");
 
-            //Setzt den wert "wurdeByeEingeben" standardmäßig auf false
+            //Setzt den Wert "wurdeByeEingeben" standardmäßig auf false
             bool wurdeByeEingegeben = false;
             do
             {
-                Console.WriteLine("Wie kann ich ihnen Helfen?");
-                //Deklariere einen Stringname
+                Console.WriteLine("Wie kann ich Ihnen helfen?");
+                //Deklariere einen String
                 string benutzereingabe;
-                //Wiederholt die Schleife solange keine Eingabe getätigt wird
+                //Wiederholt die Schleife, solange keine Eingabe getätigt wird
                 do
                 {
-                    //Liest die Eingabe und Speichert sie
+                    //Liest die Eingabe und speichert diese
                     benutzereingabe = Console.ReadLine();
-                    //Kontrolliert auf Leer
+                    //Kontrolliert auf Inhalt
                 } while (benutzereingabe == "");
 
-                //Wenn Eingabe NICHT "bye" ist geht der Code weiter
+                //Wenn Eingabe NICHT "bye" ist, geht der Code weiter
                 if (benutzereingabe != "bye")
                 {
-                    //Ein Boolean der Standartmäßig false zurück gibt
-                    //Zweck ist ob eine Eingabe für den Wert da war
+                    //Ein Boolean, der standardmäßig false zurück gibt
+                    //Zweck ist, ob eine Eingabe für den Wert da war
                     bool isAnswerAlraedyGiven;
-                    isAnswerAlraedyGiven = GivePrepartAnswerIfPossible(benutzereingabe, myDict);
+                    isAnswerAlraedyGiven = GivePreparedAnswerIfPossible(benutzereingabe, myDict);
 
                     //Wenn noch keine Aussage ausgeben wurde
                     if (!isAnswerAlraedyGiven)
                     {
-                        //Gibt eine Standardaussage aus
+                        //Gibt eine Standardausgabe aus
                         Console.WriteLine("Erro 404");
                     }
                 } //end if 
                 else
                 {
-                    //Wenn  werd eingegeben worden ist, ist die Variable true 
+                    //Wenn "Bye" eingegeben worden ist, ist die Variable true 
                     wurdeByeEingegeben = true;
                 }
-                //Wenn bye eingegeben worden ist  wird die Schleife unterbrochen und das Programm ist Beendet
+                //Wenn "Bye" eingegeben worden ist,  wird die Schleife unterbrochen und das Programm ist beendet
             } while (!wurdeByeEingegeben);
         }
 
@@ -54,30 +54,30 @@ namespace Chatbot
         /// Versucht eine vorgespeicherte Antwort für die Benutzereingabe herauszugeben 
         /// </summary>
         /// <param name="benutzereingabe"></param>
-        /// <param name="myDict">Gefühltes Dictionary mit Schlüsselwörter und Ausgaben</param>
-        /// <returns>Wurde eine Aussage gefunden?</returns>
-        private static bool GivePrepartAnswerIfPossible(string benutzereingabe, Dictionary<string, string> myDict)
+        /// <param name="myDict">gefülltes Dictionary mit Schlüsselwörtern und Ausgaben</param>
+        /// <returns>Wurde eine Aussgabe gefunden?</returns>
+        private static bool GivePreparedAnswerIfPossible(string benutzereingabe, Dictionary<string, string> myDict)
         {
-            //Ein Boolean der Standartmäßig false zurück gibt
-            //Zweck ist ob eine Eingabe für den Wert da war
+            //Ein Boolean, der Standartmäßig false zurück gibt
+            //um zu überprüfen, ob eine Eingabe für den Wert da war
             var isAnswerAlraedyGiven = false;
             //Speichert mehrere Strings mit deren Inhalt
-            //Die Strings sind die Benutzereingabe die Aufgespaltet werden 
+            //Die Strings sind die Benutzereingabe, die aufgespaltet werden 
             string[] wordarray = benutzereingabe.Split(' ');
-            //Wiederholt etwas für jedes Wort im Arry
+            //Wiederholt etwas für jedes Wort im Array
             foreach (string word in wordarray)
             {
                 //für jedes Key Value Paar im Dictionary wiederholt etwas 
                 foreach (var dictEntrie in myDict)
                 {
                     //Gleicht aktuelles Wort mit aktuellem Schlüssel ab 
-                    //Wenn der Schlüssel und das Word Identisch sind, passiert etwas
+                    //Wenn der Schlüssel und das Word identisch sind, passiert etwas
+                    //Das ToUpper hilft bei unterschiedlicher Groß- und Kleinschreibung
                     if (dictEntrie.Key.ToUpper() == word.ToUpper())
                     {
                         //Schreibt den Wert des Schlüssels in die Konsole
                         Console.WriteLine(dictEntrie.Value);
-                        //Wenn eine passende Aussage getätigt worden ist 
-                        //Wird sie auf true gesetzt
+                        //wird auf true gesetzt, weil eine passende Ausgabe getätigt wurde
                         isAnswerAlraedyGiven = true;
                     }
                 }
@@ -87,14 +87,14 @@ namespace Chatbot
         }
 
         /// <summary>
-        /// Eine vorgefertigte Liste von Key Value Pairs
+        /// Eine vorgefertigte Liste von Key-Value-Pairs
         /// Key = Schlüsselwort
         /// Value = Botausgabe
         /// </summary>
         /// <returns></returns>
         private static Dictionary<string, string> CreateDictionary()
         {
-            //Es ist ein Nachschlagewerk wo man Key Value Pair hinzufügen und Speicher kann.
+            //Es ist ein Nachschlagewerk wo man Key-Value-Pair hinzufügen und speichern kann.
             var myDict = new Dictionary<string, string>
             {
                 {"Festplatte", "Garantie"},
